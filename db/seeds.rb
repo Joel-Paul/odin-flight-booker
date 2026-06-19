f33175cf01e819a7562ec97c5d2f7aa88c4fdfe6
Joel Paul <joel.v.paul@outlook.com>
2026-06-19 12:59:43 +1000

    Seed database with Australian international airports

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,91 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Airport.delete_all
+Airport.create!([
+  {
+    IATA: "ADL",
+    name: "Adelaide Airport",
+    city: "Adelaide",
+    country: "Australia"
+  },
+  {
+    IATA: "BNE",
+    name: "Brisbane Airport",
+    city: "Brisbane",
+    country: "Australia"
+  },
+  {
+    IATA: "BME",
+    name: "Broome International Airport",
+    city: "Broome",
+    country: "Australia"
+  },
+  {
+    IATA: "CNS",
+    name: "Cairns Airport",
+    city: "Cairns",
+    country: "Australia"
+  },
+  {
+    IATA: "CBR",
+    name: "Canberra Airport",
+    city: "Canberra",
+    country: "Australia"
+  },
+  {
+    IATA: "DRW",
+    name: "Darwin International Airport",
+    city: "Darwin",
+    country: "Australia"
+  },
+  {
+    IATA: "OOL",
+    name: "Gold Coast Airport",
+    city: "Gold Coast",
+    country: "Australia"
+  },
+  {
+    IATA: "HBA",
+    name: "Hobart Airport",
+    city: "Hobart",
+    country: "Australia"
+  },
+  {
+    IATA: "HID",
+    name: "Horn Island Airport",
+    city: "Horn Island",
+    country: "Australia"
+  },
+  {
+    IATA: "MEL",
+    name: "Melbourne Airport",
+    city: "Melbourne",
+    country: "Australia"
+  },
+  {
+    IATA: "NTL",
+    name: "Newcastle Airport",
+    city: "Newcastle",
+    country: "Australia"
+  },
+  {
+    IATA: "PER",
+    name: "Perth Airport",
+    city: "Perth",
+    country: "Australia"
+  },
+  {
+    IATA: "MCY",
+    name: "Sunshine Coast Airport",
+    city: "Sunshine Coast",
+    country: "Australia"
+  },
+  {
+    IATA: "SYD",
+    name: "Sydney Airport",
+    city: "Sydney",
+    country: "Australia"
+  }
+])
