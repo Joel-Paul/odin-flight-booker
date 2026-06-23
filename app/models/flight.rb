d8669592db8861bb653dd50ad45bb9feed6758dd
Joel Paul <joel.v.paul@outlook.com>
2026-06-23 11:20:42 +1000

    Order flights by departure time by default

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -4,4 +4,6 @@ class Flight < ApplicationRecord
 
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
+
+  default_scope { order(takeoff: :asc) }
 end
