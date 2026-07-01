a48fe212d2ff2276820e794edfadd5bd974b7bdd
Joel Paul <joel.v.paul@outlook.com>
2026-07-01 11:15:13 +1000

    Allow removing passengers when booking

--- app/javascript/controllers/passenger_controller.js
+++ app/javascript/controllers/passenger_controller.js
@@ -1,10 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "list", "template" ]
+  static targets = [ "list", "template", "passenger" ]
 
   add() {
     const passenger_field = document.importNode(this.templateTarget.content, true);
-    this.listTarget.appendChild(passenger_field)
+    this.listTarget.appendChild(passenger_field);
+  }
+
+  remove() {
+    if (this.passengerTargets.length > 1) {
+        const passenger = this.passengerTargets.at(-1);
+        passenger.remove()
+    }
   }
 }
